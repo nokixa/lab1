10d826fb06828dd512cbe771899e10af53d5414b
ΧΑΧΑΛΗΣ ΝΙΚΟΛΑΟΣ <up1097287>
2025-02-24 13:03:34 +0200

    cookies

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import requests
+from time import datetime
 
 def more(text):
     count = 0
@@ -28,3 +29,5 @@ with requests.get(url) as response:
     print(f"Server: {response.headers.get('Server')}")
 #   2o
     print(f"Has cookies: {'Set-Cookie' in response.headers}")
+    for cookie in response.cookies:
+        print(f"Name: {cookie.name}, Expires: {datetime.fromtimestamp(cookie.expires)}")
